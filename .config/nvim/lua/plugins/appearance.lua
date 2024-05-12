131e61ad6ec5c5c285d2d62b5f771ae80d82c2ed
nandrez <nahuel.perez@gmx.ch>
2024-05-12 16:26:42 +0200

    Move telescope nvim-plugin to 'plugins/search.lua'
    - extracted from 'plugins/appearance.lua'

--- .config/nvim/lua/plugins/appearance.lua
+++ .config/nvim/lua/plugins/appearance.lua
@@ -12,31 +12,6 @@ return {
 		end,
 	},
 	{
-		'nvim-telescope/telescope.nvim',
-		branch = '0.1.x',
-		dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope-ui-select.nvim' },
-		config = function()
-			local telescope = require('telescope')
-			telescope.setup({
-				extensions = {
-					['ui-select'] = {
-						require('telescope.themes').get_dropdown({}),
-					},
-				},
-			})
-			pcall(telescope.load_extension, 'ui-select')
-			pcall(telescope.load_extension, 'fzf')
-
-			local builtin = require('telescope.builtin')
-			vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-			vim.keymap.set('n', '<leader>sgf', builtin.git_files, { desc = '[S]earch [G]it [F]iles' })
-			vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
-			vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-			vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
-			vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-		end,
-	},
-	{
 		'folke/noice.nvim',
 		event = 'VeryLazy',
 		dependencies = {
---
+++ .config/nvim/lua/plugins/search.lua
@@ -0,0 +1,35 @@
+return {
+	---
+	{
+		'nvim-telescope/telescope.nvim',
+		branch = '0.1.x',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'nvim-telescope/telescope-ui-select.nvim',
+			'smartpde/telescope-recent-files',
+		},
+		config = function()
+			local telescope = require('telescope')
+			telescope.setup({
+				extensions = {
+					['ui-select'] = {
+						require('telescope.themes').get_dropdown({}),
+					},
+				},
+			})
+			pcall(telescope.load_extension, 'fzf')
+			pcall(telescope.load_extension, 'ui-select')
+			pcall(telescope.load_extension, 'telescope-recent-files')
+
+			local builtin = require('telescope.builtin')
+			vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+			vim.keymap.set('n', '<leader>sgf', builtin.git_files, { desc = '[S]earch [G]it [F]iles' })
+			vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+			vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+			vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+			vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+			local recent_files = telescope.extensions.recent_files
+			vim.keymap.set('n', '<leader>sr', recent_files.pick, { desc = '[S]earch [R]ecent files' })
+		end,
+	},
+}

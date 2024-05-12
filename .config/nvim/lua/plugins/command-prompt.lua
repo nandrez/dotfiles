3070b4d505afa8bf85ee2d8b9bf4c9086b7a023f
nandrez <nahuel.perez@gmx.ch>
2024-05-12 17:55:02 +0200

    Fix nvim-notify background warning
    - since it does not seem to be able to deal with
      transparent backgrounds

--- .config/nvim/lua/plugins/command-prompt.lua
+++ .config/nvim/lua/plugins/command-prompt.lua
@@ -5,10 +5,12 @@ return {
 		dependencies = {
 			-- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
 			'MunifTanjim/nui.nvim',
-			-- OPTIONAL:
-			--   `nvim-notify` is only needed, if you want to use the notification view.
-			--   If not available, we use `mini` as the fallback
-			'rcarriga/nvim-notify',
+			{
+				'rcarriga/nvim-notify',
+				opts = {
+					background_colour = '#313244',
+				},
+			},
 		},
 		opts = {
 			lsp = {

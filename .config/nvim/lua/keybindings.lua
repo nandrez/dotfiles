b19212d6d16d8e0f70256f9de45129f98a06be94
nandrez <nahuel.perez@gmx.ch>
2024-05-12 16:56:12 +0200

    Add bindings for nvim-buffer navigation

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -1,3 +1,7 @@
 -- LEADER KEY
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
+
+-- NAVIGATION
+vim.keymap.set('n', '<leader>bn', ':bnext<CR>', { desc = 'Go to [N]ext buffer' })
+vim.keymap.set('n', '<leader>bp', ':bprev<CR>', { desc = 'Go to [P]revious buffer' })

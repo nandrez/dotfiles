f1a2fa415789ebb49946172baa3c563fd048c322
nandrez <nahuel.perez@gmx.ch>
2025-01-03 23:16:54 +0100

    Fix duplicate keybinding for searching git-files

--- .config/nvim/lua/plugins/search.lua
+++ .config/nvim/lua/plugins/search.lua
@@ -23,7 +23,7 @@ return {
 
 			local builtin = require('telescope.builtin')
 			vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
-			vim.keymap.set('n', '<leader>sgf', builtin.git_files, { desc = '[S]earch [G]it [F]iles' })
+			vim.keymap.set('n', '<leader>sG', builtin.git_files, { desc = '[S]earch [G]it [F]iles' })
 			vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
 			vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 			vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
